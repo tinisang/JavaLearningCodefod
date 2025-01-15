c3b184b0594e5dd5da1cb5997773b98d8b14cdcc
tinisang <spham8011@gmail.com>
2025-01-15 23:12:51 +0700

    Update Array Practice

---
+++ Week1+2_Java_Basic_Concepts/CustomPackage/ArrayPractice.java
@@ -0,0 +1,15 @@
+package CustomPackage;
+
+public class ArrayPractice {
+
+    public static void main(String[] args) {
+        int[] arr = new int[4];
+        arr[0] = 2;
+        arr[1] = 4;
+        arr[2] = 6;
+        arr[3] = 8;
+
+
+        System.out.println(arr[3]);
+    }
+}
